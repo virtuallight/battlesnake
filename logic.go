ef7c8d2752c42ca29a9abec6cb44c76fbb6bee62
Mateusz Jankowski <virtuallight@users.noreply.github.com>
2021-08-26 10:02:52 +0000

    Update BattlesnakeInfoResponse

--- logic.go
+++ logic.go
@@ -19,10 +19,10 @@ func info() BattlesnakeInfoResponse {
 	log.Println("INFO")
 	return BattlesnakeInfoResponse{
 		APIVersion: "1",
-		Author:     "",        // TODO: Your Battlesnake username
-		Color:      "#888888", // TODO: Personalize
-		Head:       "default", // TODO: Personalize
-		Tail:       "default", // TODO: Personalize
+		Author:     "virtuallight",
+		Color:      "#88ff88", // TODO: Personalize
+		Head:       "smile", // TODO: Personalize
+		Tail:       "bolt", // TODO: Personalize
 	}
 }
 
