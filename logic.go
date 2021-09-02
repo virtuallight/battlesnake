22039f558d8f3a9f670000261d74a0139c056be3
Mateusz Jankowski <mat.jankowski@gmail.com>
2021-09-02 12:19:13 +0300

    Don't hit obstacles: self, others, walls

--- logic.go
+++ logic.go
@@ -20,9 +20,9 @@ func info() BattlesnakeInfoResponse {
 	return BattlesnakeInfoResponse{
 		APIVersion: "1",
 		Author:     "virtuallight",
-		Color:      "#88ff88", // TODO: Personalize
-		Head:       "smile", // TODO: Personalize
-		Tail:       "bolt", // TODO: Personalize
+		Color:      "#88ff88",
+		Head:       "smile",
+		Tail:       "bolt",
 	}
 }
 
@@ -43,37 +43,40 @@ func end(state GameState) {
 // where to move -- valid moves are "up", "down", "left", or "right".
 // We've provided some code and comments to get you started.
 func move(state GameState) BattlesnakeMoveResponse {
+
+	myHead := state.You.Head
+	boardWidth := state.Board.Width
+	boardHeight := state.Board.Height
+
+	// Step 0 - Init `possibleMoves` map
+	// Step 1 - Don't hit walls.
 	possibleMoves := map[string]bool{
-		"up":    true,
-		"down":  true,
-		"left":  true,
-		"right": true,
+		"up":    myHead.Y < boardHeight-1,
+		"down":  myHead.Y > 0,
+		"left":  myHead.X > 0,
+		"right": myHead.X < boardWidth-1,
 	}
 
-	// Step 0: Don't let your Battlesnake move back in on it's own neck
-	myHead := state.You.Body[0] // Coordinates of your head
-	myNeck := state.You.Body[1] // Coordinates of body piece directly behind your head (your "neck")
-	if myNeck.X < myHead.X {
-		possibleMoves["left"] = false
-	} else if myNeck.X > myHead.X {
-		possibleMoves["right"] = false
-	} else if myNeck.Y < myHead.Y {
-		possibleMoves["down"] = false
-	} else if myNeck.Y > myHead.Y {
-		possibleMoves["up"] = false
+	// Step 2 - Don't hit yourself.
+	// Step 3 - Don't collide with others.
+	toTheLeft := Coord{X: myHead.X - 1, Y: myHead.Y}
+	toTheRight := Coord{X: myHead.X + 1, Y: myHead.Y}
+	toTheTop := Coord{X: myHead.X, Y: myHead.Y + 1}
+	toTheBottom := Coord{X: myHead.X, Y: myHead.Y - 1}
+
+	for _, snake := range state.Board.Snakes {
+		for _, bodyPartCoord := range snake.Body {
+			if bodyPartCoord == toTheLeft {
+				possibleMoves["left"] = false
+			} else if bodyPartCoord == toTheRight {
+				possibleMoves["right"] = false
+			} else if bodyPartCoord == toTheTop {
+				possibleMoves["up"] = false
+			} else if bodyPartCoord == toTheBottom {
+				possibleMoves["down"] = false
+			}
+		}
 	}
-
-	// TODO: Step 1 - Don't hit walls.
-	// Use information in GameState to prevent your Battlesnake from moving beyond the boundaries of the board.
-	// boardWidth := state.Board.Width
-	// boardHeight := state.Board.Height
-
-	// TODO: Step 2 - Don't hit yourself.
-	// Use information in GameState to prevent your Battlesnake from colliding with itself.
-	// mybody := state.You.Body
-
-	// TODO: Step 3 - Don't collide with others.
-	// Use information in GameState to prevent your Battlesnake from colliding with others.
 
 	// TODO: Step 4 - Find food.
 	// Use information in GameState to seek out and find food.
